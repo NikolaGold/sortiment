6a3adce78207af5da196238a0edd7d52aa666b3b
NikolaGold <nikola.goldova1@gmail.com>
2018-10-17 21:12:29 +0200

    prepare table

--- main.js
+++ main.js
@@ -1,7 +1,46 @@
+
+
 function renderStockTable() {
-    // nahraď svojou implementáciou
-    alert('No data to show, Saphira hasn\'t implemented this functionality yet!')
-}
+ 		let table = "<table>";
+ 		let header = prepareTableHeader();
+ 		table += header;
+
+ 		for (i = 0; i < stock.length; i++) {
+ 			table += prepareTableRow(i);
+ 		}
+ 	
+ 		table += "</table>";
+
+ 		document.getElementById("table-section").innerHTML = table;
+ }
+
+ function prepareTableHeader() {
+ 	 	let header = "<tr>";
+
+ 		let titles = ["Název", "Kategorie", "Jednotka", "Cena"];
+
+ 		for (let i = 0; i < 4 ; i++){
+ 			header += "<th>" + titles[i] + "</th>"
+ 		}
+
+ 		header += "</tr>";
+ 		
+ 		return header;
+ }
+
+ function prepareTableRow(i) {
+  		let row = "<tr>";
+ 		row += "<td>" + stock[i].name + "</td>";
+ 		row += "<td>" + stock[i].category + "</td>";
+ 		row += "<td>" + stock[i].price + "</td>";
+ 		row += "<td>" + stock[i].unit + "</td>";
+ 		row += "</tr>";
+
+ 		return row;
+ }
+
+
+
 
 // nasledujúci príkaz nemeň, ale inak si rob v celom súbore, čo chceš =)
 window.onload = () => {
