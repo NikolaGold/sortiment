4f69a267695bea103258ecda97b27be6b6732ecb
NikolaGold <nikola.goldova1@gmail.com>
2018-10-17 19:50:04 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+function renderStockTable() {
+    // nahraď svojou implementáciou
+    alert('No data to show, Saphira hasn\'t implemented this functionality yet!')
+}
+
+// nasledujúci príkaz nemeň, ale inak si rob v celom súbore, čo chceš =)
+window.onload = () => {
+    renderStockTable(stock);
+}
